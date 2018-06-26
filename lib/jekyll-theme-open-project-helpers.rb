8ff47c1961e3bc44eaed536cf5e2d114ef893b3b
Anton Strogonoff <anton@strogonoff.name>
2018-06-26 23:27:00 +0700

    feat: page.items for unfiltered index pages

--- lib/jekyll-theme-open-project-helpers.rb
+++ lib/jekyll-theme-open-project-helpers.rb
@@ -14,12 +14,26 @@ module Jekyll
 end
 
 module OpenProjectHelpers
+
   # On an open hub site, Jekyll Open Project theme assumes the existence of two types
   # of item indexes: software and specs, where items are gathered
   # from across open projects in the hub.
   #
-  # Each item can have its tags, and the theme allows to filter each index by a tag.
-  # To facilitate that, the below generates an additional index page
+  # The need for :item_test arises from our data structure (see Jekyll Open Project theme docs)
+  # and the fact that Jekyll doesn’t intuitively handle nested collections.
+  INDEXES = {
+    "software" => {
+      :item_test => lambda { |item| item.url.include? '_software' and not item.url.include? '_docs' },
+    },
+    "specs" => {
+      :item_test => lambda { |item| item.url.include? '_specs' and not item.url.include? '_docs' },
+    },
+  }
+
+
+  # Each software or spec item can have its tags,
+  # and the theme allows to filter each index by a tag.
+  # The below generates an additional index page
   # for each tag in an index, like software/Ruby.
   #
   # Note: this expects "_pages/<index page>.html" to be present in site source,
@@ -48,18 +62,7 @@ module OpenProjectHelpers
       # a Jekyll Open Project hub site.
       if site.collections.key? 'projects'
 
-        # The need for :item_test arises from our data structure (see Jekyll Open Project theme docs)
-        # and the fact that Jekyll doesn’t intuitively handle nested collections.
-        indexes = {
-          "software" => {
-            :item_test => lambda { |item| item.url.include? '_software' },
-          },
-          "specs" => {
-            :item_test => lambda { |item| item.url.include? '_specs' },
-          },
-        }
-
-        indexes.each do |index_name, params|
+        INDEXES.each do |index_name, params|
           items = site.collections['projects'].docs.select { |item| params[:item_test].call(item) }
 
           # Creates a data structure like { tag1: [item1, item2], tag2: [item2, item3] }
@@ -86,6 +89,41 @@ module OpenProjectHelpers
               tagged_items,
               index_name)
           end
+        end
+      end
+    end
+  end
+
+
+  # Below passes the `items` variable to normal (unfiltered)
+  # index page layout.
+
+  class IndexPage < Jekyll::Page
+    def initialize(site, base, dir, items, index_page)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_pages'), "#{index_page}.html")
+      self.data['items'] = items
+    end
+  end
+
+  class IndexPageGenerator < Jekyll::Generator
+    safe true
+
+    def generate(site)
+
+      # If there’s a “projects” collection, we assume it is indeed
+      # a Jekyll Open Project hub site.
+      if site.collections.key? 'projects'
+
+        INDEXES.each do |index_name, params|
+          items = site.collections['projects'].docs.select { |item| params[:item_test].call(item) }
+          page = site.site_payload["site"]["pages"].detect { |p| p.url == "/#{index_name}/" }
+          page.data['items'] = items
         end
       end
     end
