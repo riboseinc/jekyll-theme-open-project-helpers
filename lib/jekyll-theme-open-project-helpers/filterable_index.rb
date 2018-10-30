c1d7ddff3d7509dc83a6bf36800177e5f13256c9
Anton Strogonoff <anton@strogonoff.name>
2018-10-30 21:05:04 +0800

    fix: Unbreak software/spec item links on hub site

--- lib/jekyll-theme-open-project-helpers/filterable_index.rb
+++ lib/jekyll-theme-open-project-helpers/filterable_index.rb
@@ -45,7 +45,7 @@ module Jekyll
       def generate(site)
         if site.config['is_hub']
           INDEXES.each do |index_name, params|
-            items = site.collections['projects'].docs.select { |item| params[:item_test].call(item) }
+            items = get_all_items(site, 'projects', params[:item_test])
 
             # Creates a data structure like { tag1: [item1, item2], tag2: [item2, item3] }
             tags = {}
@@ -100,19 +100,7 @@ module Jekyll
             # Filters items from given collection_name through item_test function
             # and makes items available in templates via e.g. site.all_specs, site.all_software
 
-            items = site.collections[collection_name].docs.select { |item| params[:item_test].call(item) }
-
-            if site.config['is_hub']
-              items.map! do |item|
-                project_name = item.url.split('/')[2]
-                project_path = "_projects/#{project_name}/index.md"
-
-                item.data[:project_name] = project_name
-                item.data[:project_data] = site.collections['projects'].docs.select { |proj| proj.path.end_with? project_path } [0]
-
-                item
-              end
-            end
+            items = get_all_items(site, collection_name, params[:item_test])
 
             site.config["all_#{index_name}"] = items
             site.config["num_all_#{index_name}"] = items.size
@@ -130,4 +118,29 @@ module Jekyll
     end
 
   end
+end
+
+
+def get_all_items(site, collection_name, filter_func)
+  # Fetches items of specified type, ordered and prepared for usage in index templates
+
+  items = site.collections[collection_name].docs.select { |item|
+    filter_func.call(item)
+  }
+
+  if site.config['is_hub']
+    items.map! do |item|
+      project_name = item.url.split('/')[2]
+      project_path = "_projects/#{project_name}/index.md"
+
+      item.data[:project_name] = project_name
+      item.data[:project_data] = site.collections['projects'].docs.select {
+        |proj| proj.path.end_with? project_path
+      } [0]
+
+      item
+    end
+  end
+
+  return items
 end
