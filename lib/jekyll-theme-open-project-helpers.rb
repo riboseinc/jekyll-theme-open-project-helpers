f16c230bd027f2946af387e079353850002dc186
Anton Strogonoff <anton@strogonoff.name>
2018-11-01 16:23:21 +0800

    feat: Mark external links within main site content

--- lib/jekyll-theme-open-project-helpers.rb
+++ lib/jekyll-theme-open-project-helpers.rb
@@ -7,6 +7,7 @@ require 'jekyll-theme-open-project-helpers/site_type'
 require 'jekyll-theme-open-project-helpers/project_data_reader'
 require 'jekyll-theme-open-project-helpers/filterable_index'
 require 'jekyll-theme-open-project-helpers/blog_index'
+require 'jekyll-theme-open-project-helpers/external_links'
 
 
 module Jekyll
---
+++ lib/jekyll-theme-open-project-helpers/external_links.rb
@@ -0,0 +1,27 @@
+require 'nokogiri'
+require 'uri'
+
+def process_content(site_hostname, content)
+  content = Nokogiri::HTML(content)
+  content.css('main a').each do |a|
+    next unless a.get_attribute('href') =~ /\Ahttp/i
+    next if a.get_attribute('href').include? site_hostname
+    a.set_attribute('rel', 'external')
+    a.inner_html = "#{a.inner_html}<span class='ico-ext'><i class='fas fa-external-link-square'></i></span>"
+  end
+  return content.to_s
+end
+
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  site_hostname = URI(doc.site.config['url']).host
+  unless doc.asset_file?
+    doc.output = process_content(site_hostname, doc.output)
+  end
+end
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+  site_hostname = URI(page.site.config['url']).host
+  unless page.asset_file?
+    page.output = process_content(site_hostname, page.output)
+  end
+end
