4c1350447c32899b1ca1bc264ff52f02abab07fa
Anton Strogonoff <anton@strogonoff.name>
2019-06-06 18:23:51 +0800

    feat: Remove external links marking functionality
    in favor of jekyll-external-links plugin
    fix #17

--- lib/jekyll-theme-open-project-helpers.rb
+++ lib/jekyll-theme-open-project-helpers.rb
@@ -7,7 +7,6 @@ require 'jekyll-theme-open-project-helpers/site_type'
 require 'jekyll-theme-open-project-helpers/project_data_reader'
 require 'jekyll-theme-open-project-helpers/filterable_index'
 require 'jekyll-theme-open-project-helpers/blog_index'
-require 'jekyll-theme-open-project-helpers/external_links'
 require 'jekyll-theme-open-project-helpers/spec_builders/spec_builder'
 require 'jekyll-theme-open-project-helpers/static_file_fix'
 
